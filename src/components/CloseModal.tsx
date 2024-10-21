83fc05afeb0715198b4c7b0cbfc6e91d7487a0b4
alt-plus-f4 <valentin.assenov@abv.bg>
2024-10-21 14:53:58 +0300

    fix: move dependency and edit type of closeModal

--- src/components/CloseModal.tsx
+++ src/components/CloseModal.tsx
@@ -1,14 +1,14 @@
 "use client"
 
 import { X } from "lucide-react";
-import { Button } from "@components/ui/button";
+import { Button } from "./ui/button";
 import { useRouter } from "next/navigation";
 
 const CloseModal = () => {
     const router = useRouter()
 
     return (
-        <Button variant='subtle' className="h-6 w-6 p-0 rounded-md" onClick={() => router.back()}>
+        <Button variant='ghost' className="h-6 w-6 p-0 rounded-md" onClick={() => router.back()}>
             <X className="h-4 w-4"/>
         </Button>
     );
