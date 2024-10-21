f5585e0b86c4b11ff9470bc8203652b1d0281138
alt-plus-f4 <valentin.assenov@abv.bg>
2024-10-21 14:46:53 +0300

    fix: fix linter errors

--- src/components/UserNav.tsx
+++ src/components/UserNav.tsx
@@ -2,7 +2,6 @@ import { User } from "next-auth";
 import {
   Avatar,
   AvatarFallback,
-  AvatarImage,
 } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import {
