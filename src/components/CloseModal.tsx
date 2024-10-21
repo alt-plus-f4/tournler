5554aa3930270a8d80dc9506fd7defed9f961366
alt-plus-f4 <valentin.assenov@abv.bg>
2024-10-21 14:52:25 +0300

    chore: edit eslinter config to include next errors and not only ts

--- src/components/CloseModal.tsx
+++ src/components/CloseModal.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { X } from "lucide-react";
-import { Button } from "./Button";
+import { Button } from "@components/ui/button";
 import { useRouter } from "next/navigation";
 
 const CloseModal = () => {
