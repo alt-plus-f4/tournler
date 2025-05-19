4e693905b6865fa5deca46c6059ac8445c3ea46d
alt-plus-f4 <valentin.assenov@abv.bg>
2025-05-19 21:43:45 +0300

    fix: unified language

--- src/components/onboarding/WelcomeStep.tsx
+++ src/components/onboarding/WelcomeStep.tsx
@@ -6,21 +6,23 @@ import { DialogFooter, DialogTitle } from '../ui/dialog';
 import { Button } from '../ui/button';
 
 interface WelcomeStepProps {
-  nextStep: () => void;
+	nextStep: () => void;
 }
 
 export function WelcomeStep({ nextStep }: WelcomeStepProps) {
-  return (
-    <div className='p-24'>
-      <div className="flex flex-col items-center text-center w-full">
-        <Icons.logo size={128} />
-        <DialogTitle className="text-xl font-bold mt-4 mb-2">Добре Дошли в Tues gaming</DialogTitle>
-      </div>
-      <DialogFooter className="flex mt-4 justify-center">
-        <Button variant="default" onClick={nextStep}>
-          Продължи
-        </Button>
-      </DialogFooter>
-    </div>
-  );
+	return (
+		<div className='p-24'>
+			<div className='flex flex-col items-center text-center w-full'>
+				<Icons.logo size={128} />
+				<DialogTitle className='text-xl font-bold mt-4 mb-2'>
+					Welcome to Tues gaming
+				</DialogTitle>
+			</div>
+			<DialogFooter className='flex mt-4 justify-center'>
+				<Button variant='default' onClick={nextStep}>
+					Continue
+				</Button>
+			</DialogFooter>
+		</div>
+	);
 }
