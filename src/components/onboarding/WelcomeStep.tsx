aca2b3240c0a00020619d70bab55f4cb00981cca
alt-plus-f4 <valentin.assenov@abv.bg>
2025-05-19 21:45:29 +0300

    chore: merge changes

--- src/components/onboarding/WelcomeStep.tsx
+++ src/components/onboarding/WelcomeStep.tsx
@@ -7,11 +7,10 @@ import { Button } from '../ui/button';
 
 interface WelcomeStepProps {
 	nextStep: () => void;
-  nextStep: () => void;
-  loading: boolean;
+	loading: boolean;
 }
 
-export function WelcomeStep({ nextStep }: WelcomeStepProps) {
+export function WelcomeStep({ nextStep, loading }: WelcomeStepProps) {
 	return (
 		<div className='p-24'>
 			<div className='flex flex-col items-center text-center w-full'>
@@ -21,26 +20,10 @@ export function WelcomeStep({ nextStep }: WelcomeStepProps) {
 				</DialogTitle>
 			</div>
 			<DialogFooter className='flex mt-4 justify-center'>
-				<Button variant='default' onClick={nextStep}>
-					Continue
+				<Button variant='default' onClick={nextStep} disabled={loading}>
+					{loading ? 'Loading...' : 'Продължи'}
 				</Button>
 			</DialogFooter>
 		</div>
 	);
-export function WelcomeStep({ nextStep, loading }: WelcomeStepProps) {
-  return (
-    <div className='p-24'>
-      <div className="flex flex-col items-center text-center w-full">
-        <Icons.logo size={128} />
-        <DialogTitle className="text-xl font-bold mt-4 mb-2">
-          Добре Дошли в Tues gaming
-        </DialogTitle>
-      </div>
-      <DialogFooter className="flex mt-4 justify-center">
-        <Button variant="default" onClick={nextStep} disabled={loading}>
-          {loading ? 'Loading...' : 'Продължи'}
-        </Button>
-      </DialogFooter>
-    </div>
-  );
 }
